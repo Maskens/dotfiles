acfd59d0430ecade9fcce2870d31cf6e6a2eefa8
Magnus Stenqvist <magnus.p.stenqvist@gmail.com>
2025-02-02 11:33:42 +0100

    WIP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,6 +14,7 @@ if not vim.uv.fs_stat(lazypath) then
     "--branch=stable",
     lazypath
   }
+
 end
 
 vim.opt.rtp:prepend(lazypath)
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -38,20 +38,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
-    end,
-  },
-  {
-    "S1M0N38/love2d.nvim",
-    cmd = "LoveRun",
-    opts = {
-    },
-    keys = {
-      { "<leader>v", ft = "lua", desc = "LÖVE" },
-      { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
-      { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
-    },
+    end
   }
-
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
